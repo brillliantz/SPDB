6ab3bbce62d36317f936adcb9a39357db984f513
William <brillliantz@outlook.com>
2017-02-09 21:39:55 +0800

    Add constructor, getName, getData, operator[] of class DataColumn.

--- SPDB/DataFrame.cpp
+++ SPDB/DataFrame.cpp
@@ -9,6 +9,9 @@
 #include <unordered_map>
 
 
+/**
+ * Index implementation:
+ */
 void Index::add(std::string &name, DBData::DBDataType type) {
     indices.push_back(name);
     types.push_back(type);
@@ -23,3 +26,21 @@ std::string Index::operator[](int i) const {
         return indices[indices.size() + i];
     }
 }
+
+/**
+ * DataColumn implementation:
+ */
+DataColumn::DataColumn(std::string name, std::vector<DBData> data) :
+name_{name} {
+    std::vector<DBData*> data_ {};
+    for (const auto& d : data) {
+        data_.push_back(&d);
+    }
+}
+
+std::string DataColumn::getName() const { return name_; }
+
+std::vector<DBData*>& DataColumn::getData() const { return data_; }
+
+DBData& DataColumn::operator[](int i) { return *(data_[i]); }
+
